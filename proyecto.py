a4e60b86357933be97374b1a48dbfc4afc9cf10e
dandrew97 <dcampos823@gmail.com>
2024-02-05 17:30:38 -0500

    feat: :hammer: Se agrega la función de editar contacto
    Se agrega la función de editar contacto, la cual permite editar la informacion de los contactos que anteriormente ha sido guardada. Esta función en primera instancia verifica si existe el archivo con el nombre que se ingresa, despues solicita la información a cambiar, los cambia y cambia el nombre del archivo al nombre ingresado.

--- proyecto.py
+++ proyecto.py
@@ -6,6 +6,7 @@ EXTENSION = '.txt' #Extensión de archivos
 
 #Clase Contactos
 class Contacto:
+    #Constructor
     def __init__(self, nombre, telefono, categoria):
         self.nombre = nombre
         self.telefono = telefono
@@ -28,7 +29,7 @@ def app():
             agregar_contacto()
             preguntar = False
         elif opcion == 2:
-            print('Editar Contacto')
+            editar_contacto()
             preguntar = False
         elif opcion == 3:
             print('Ver Contactos')
@@ -41,6 +42,42 @@ def app():
             preguntar = False
         else:
             print('Opción no válida, intente de nuevo')
+
+#Función para editar un contacto existente 
+def editar_contacto():
+    print('Escribe el nombre del contacto a editar')
+    nombre_anterior = input('Nombre del contacto que desea editar: \r\n')
+
+    #Revisar si el archivo con el nombre de contacto ingresado existe
+    existe = existe_contacto(nombre_anterior)
+
+    if existe:
+        with open(CARPETA + nombre_anterior + EXTENSION, 'w') as archivo:
+
+            # Editar los campos solicitados - Estas son variables que almacenan los datos que ingrese el usuario
+            nombre_contacto = input('Agrega el nuevo nombre:\r\n')
+            telefono_contacto = input('Agrega el nuevo número telefónico:\r\n')
+            categoria_contacto = input('Agrega la nueva Categoría:\r\n')
+
+            #Instanciar clase
+            contacto = Contacto(nombre_contacto, telefono_contacto, categoria_contacto)
+
+            # Renombrar el archivo
+            # os.rename(CARPETA + nombre_anterior + EXTENSION, CARPETA + nombre_contacto + EXTENSION)
+
+            # Escribir en el archivo 
+            archivo.write('Nombre: ' + contacto.nombre + '\r')
+            archivo.write('Telefono: ' + contacto.telefono + '\r')
+            archivo.write('Categoria: ' + contacto.categoria + '\r')    
+
+            # Mostrar mensaje de exito 
+            print('\r\n Contacto editado correctamente \r\n')
+
+    else:
+        print('Este contacto no existe')
+
+    # Reiniciar la app
+    app()
 
 #Función para agregar un nuevo contacto 
 def agregar_contacto():
@@ -65,6 +102,7 @@ def agregar_contacto():
             archivo.write('Nombre: ' + contacto.nombre + '\r')
             archivo.write('Telefono: ' + contacto.telefono + '\r')
             archivo.write('Categoria: ' + contacto.categoria + '\r')
+            
 
             #Mostrar un mensaje de éxito 
             print('\r\n Contacto creado correctamente \r\n')
@@ -89,4 +127,8 @@ def crear_directorio():
     if not os.path.exists(CARPETA):
         os.makedirs(CARPETA)
 
+#Función que retorna un valor para revisar si el contacto existe o no; esta funcion es usada en la línea 52
+def existe_contacto(nombre):
+    return os.path.isfile(CARPETA + nombre + EXTENSION)
+    
 app()
